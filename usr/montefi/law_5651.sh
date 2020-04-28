a8f1fb21fc31cf6fc98541ebfebdfa9c0caff129
Soysal Tan <soysal@airsqreen.com>
2020-04-29 02:56:49 +0300

    Remove endpoint prefix

--- usr/montefi/law_5651.sh
+++ usr/montefi/law_5651.sh
@@ -3,7 +3,7 @@
 . /usr/montefi/config.sh
 . /usr/montefi/device_id.sh
 
-api="$API_BASE_URI/api/device/$DEVICE_ID/law5651"
+api="$API_BASE_URI/device/$DEVICE_ID/law5651"
 
 leases=`cat /var/dhcp.leases`
 curl --request POST --data "device_id=$device_id&leases=$leases" -D- $api
